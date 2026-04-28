c59aee1ae8e5a7aaad4a52b922efbda23e8340c3
rafaellacruzsantoss-spec <rafaellacruzsantoss@gmail.com>
2026-04-28 15:20:25 -0300

    Update teste.py

--- teste.py
+++ teste.py
@@ -1,6 +1,6 @@
 import streamlit as st
 
-st.title('Teste ECMI 2')
+st.title('')
 st.write("Seja Bem Vindo ao aplicativo mais inovador!")
 
 st.image("https://img.elo7.com.br/product/zoom/3559173/placas-boas-vindas-recepcao-casamento.jpg", width=200)
