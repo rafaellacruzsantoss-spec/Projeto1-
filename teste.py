285ccf944207aeea49b2ba174448a0ca3dcdd8a7
rafaellacruzsantoss-spec <rafaellacruzsantoss@gmail.com>
2026-04-28 15:10:50 -0300

    Update teste.py

--- teste.py
+++ teste.py
@@ -4,5 +4,5 @@ st.title('Teste ECMI 2')
 st.write("Sejam Bem Vindos ao aplicativo mais inovador!")
 import streamlit as st
 
-st.image("https://docs.streamlit.io/logo.svg", width=200)
+st.image("https://www.transportal.com.br/noticias/wp-content/uploads/2019/09/Praia-de-Gerib%C3%A1.jpg", width=200)
 
