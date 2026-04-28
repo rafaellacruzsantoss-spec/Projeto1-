5885904d581c1b9422327fff4d6720fff390ab1f
rafaellacruzsantoss-spec <rafaellacruzsantoss@gmail.com>
2026-04-28 15:09:45 -0300

    Update teste.py

--- teste.py
+++ teste.py
@@ -4,4 +4,5 @@ st.title('Teste ECMI 2')
 st.write("Sejam Bem Vindos ao aplicativo mais inovador!")
 import streamlit as st
 
-st.image("https://www.transportal.com.br/noticias/rodoviaria-novorio/praia-de-geriba-em-buzios/", width=200)
+st.image("https://docs.streamlit.io/logo.svg", width=200)
+
