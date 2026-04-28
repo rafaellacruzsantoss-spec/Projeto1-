91e958a0f7621a5085ee47c60c89db9604490b6f
rafaellacruzsantoss-spec <rafaellacruzsantoss@gmail.com>
2026-04-28 15:28:21 -0300

    Update teste.py

--- teste.py
+++ teste.py
@@ -4,4 +4,8 @@ st.title('DIVA')
 st.write("Seja Bem Vindo ao aplicativo mais inovador!")
 
 st.image("https://img.elo7.com.br/product/zoom/3559173/placas-boas-vindas-recepcao-casamento.jpg", width=200)
+from PIL import Image
+
+# Você pode passar o caminho direto como string
+st.image("https://cdn-ileapbh.nitrocdn.com/awswdmxduTjCKQiPPVuNWTjlobpOKWLT/assets/images/optimized/rev-3155ec0/aguiarbuenosaires.com/wp-content/uploads/2022/03/livraria-ateneo.jpg", caption="El Ateneo, uma das livrarias mais belas do mundo")
 
