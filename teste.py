1a2e9b4efd464a5075d5e3e64f6fd07a2d32ad08
rafaellacruzsantoss-spec <rafaellacruzsantoss@gmail.com>
2026-04-28 15:20:59 -0300

    Update teste.py

--- teste.py
+++ teste.py
@@ -1,6 +1,6 @@
 import streamlit as st
 
-st.title('')
+st.title('DIVA')
 st.write("Seja Bem Vindo ao aplicativo mais inovador!")
 
 st.image("https://img.elo7.com.br/product/zoom/3559173/placas-boas-vindas-recepcao-casamento.jpg", width=200)
