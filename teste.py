4f3874d0e9e42db8040295a76d848c6815604c71
rafaellacruzsantoss-spec <rafaellacruzsantoss@gmail.com>
2026-04-28 15:07:18 -0300

    Update teste.py

--- teste.py
+++ teste.py
@@ -2,11 +2,6 @@ import streamlit as st
 
 st.title('Teste ECMI 2')
 st.write("Sejam Bem Vindos ao aplicativo mais inovador!")
-from PIL import Image
+import streamlit as st
 
-# Você pode passar o caminho direto como string
-st.image("caminho/para/sua/foto.jpg", caption="Legenda da Imagem")
-
-# Ou abrir com a biblioteca PIL (recomendado para manipulações)
-image = Image.open('foto.jpg')
-st.image(image, caption='Minha foto local', use_container_width=True)
+st.image("https://www.transportal.com.br/noticias/rodoviaria-novorio/praia-de-geriba-em-buzios/", width=200)
