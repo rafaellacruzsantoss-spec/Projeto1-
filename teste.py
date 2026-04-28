cdc1af38d82083e05de4d4d961d049f585a24a20
rafaellacruzsantoss-spec <rafaellacruzsantoss@gmail.com>
2026-04-28 15:39:39 -0300

    Update teste.py

--- teste.py
+++ teste.py
@@ -9,3 +9,7 @@ from PIL import Image
 # Você pode passar o caminho direto como string
 st.image("https://cdn-ileapbh.nitrocdn.com/awswdmxduTjCKQiPPVuNWTjlobpOKWLT/assets/images/optimized/rev-3155ec0/aguiarbuenosaires.com/wp-content/uploads/2022/03/livraria-ateneo.jpg", caption="El Ateneo, uma das livrarias mais belas do mundo")
 
+nome = st.text_input("Digite seu nome")
+if nome:
+     st.write(nome, 'é um cara legal')
+
