0fc4575c5f869ed0bc7df45e661a0cdbbb64433a
rafaellacruzsantoss-spec <rafaellacruzsantoss@gmail.com>
2026-04-28 15:00:53 -0300

    Update teste.py

--- teste.py
+++ teste.py
@@ -1,4 +1,12 @@
 import streamlit as st
 
 st.title('Teste ECMI 2')
-st.write("Esse é o meu aplicativo, quero trabalhar nele")
+st.write("Sejam Bem Vindos ao aplicativo mais inovador!")
+from PIL import Image
+
+# Você pode passar o caminho direto como string
+st.image("caminho/para/sua/foto.jpg", caption="Legenda da Imagem")
+
+# Ou abrir com a biblioteca PIL (recomendado para manipulações)
+image = Image.open('foto.jpg')
+st.image(image, caption='Minha foto local', use_container_width=True)
