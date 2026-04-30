f85999b95fa69f46f0639fb28cdcfc1bdca65382
rafaellacruzsantoss-spec <rafaellacruzsantoss@gmail.com>
2026-04-29 23:07:25 -0300

    Update teste.py

--- teste.py
+++ teste.py
@@ -6,7 +6,7 @@ from PIL import Image
 # Você pode passar o caminho direto como string
 st.image("https://cdn-ileapbh.nitrocdn.com/awswdmxduTjCKQiPPVuNWTjlobpOKWLT/assets/images/optimized/rev-3155ec0/aguiarbuenosaires.com/wp-content/uploads/2022/03/livraria-ateneo.jpg", caption="El Ateneo, uma das livrarias mais belas do mundo")
 
-nome = st.text_input("Antes de comerçamos, gostariamos de saber o nome do usuário")
+nome = st.text_input("Antes de comerçamos, escreva seu nome:")
 if nome:
-     st.write(nome, 'muito bem!Iremos agora ')
+     st.write(nome, 'muito bem!Iremos agora conduzir para a criação da biblioteca.')
 
