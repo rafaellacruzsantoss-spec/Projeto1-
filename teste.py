08dace8dd44f05dd9db55008bedf043f5936324f
rafaellacruzsantoss-spec <rafaellacruzsantoss@gmail.com>
2026-04-30 19:55:51 -0300

    Update teste.py

--- teste.py
+++ teste.py
@@ -10,3 +10,22 @@ nome = st.text_input("Antes de comerçamos, escreva seu nome:")
 if nome:
      st.write(nome, ', muito bem!Agora iremos conduzir para a criação da biblioteca.')
 
+# Demo code sample. Not indended for production use.
+
+# See instructions for installing Requests module for Python
+# https://requests.readthedocs.io/en/master/user/install/#install
+
+import requests
+
+def execute():
+  requestUrl = "https://api.nytimes.com/svc/books/v3/lists/[DATE]/[LIST].json?api-key=[YOUR_API_KEY]"
+  requestHeaders = {
+    "Accept": "application/json"
+  }
+
+  response = requests.get(requestUrl, headers=requestHeaders)
+
+  print(response.text)
+
+if __name__ == "__main__":
+  execute()
