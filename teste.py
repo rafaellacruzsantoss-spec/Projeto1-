ee3201020fc34323fcb511a555db4f49352d853a
rafaellacruzsantoss-spec <rafaellacruzsantoss@gmail.com>
2026-04-29 23:25:37 -0300

    Update teste.py

--- teste.py
+++ teste.py
@@ -1,10 +1,10 @@
 import streamlit as st
-st.title('To be read')
-st.write("Seja bem vindo ao mais novo aplicativo que tem como intuito encontrar a sua versão literária!")
+st.title('You to know')
+st.write("Seja bem vindo ao mais novo aplicativo que analisa a estrutura da rede social na era digital!")
 from PIL import Image
 
 # Você pode passar o caminho direto como string
-st.image("https://cdn-ileapbh.nitrocdn.com/awswdmxduTjCKQiPPVuNWTjlobpOKWLT/assets/images/optimized/rev-3155ec0/aguiarbuenosaires.com/wp-content/uploads/2022/03/livraria-ateneo.jpg", caption="El Ateneo, uma das livrarias mais belas do mundo")
+st.image("https://gkpb.com.br/22302/novo-logo-youtube/", caption="Primeira rede social que permitiu a atuação do audiovisual na internet")
 
 nome = st.text_input("Antes de comerçamos, escreva seu nome:")
 if nome:
