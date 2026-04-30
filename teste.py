22c38aecd19f4ee324e2dea6f17e51a54f908acb
rafaellacruzsantoss-spec <rafaellacruzsantoss@gmail.com>
2026-04-30 20:45:16 -0300

    Update teste.py

--- teste.py
+++ teste.py
@@ -6,7 +6,7 @@ nome = st.text_input("Antes de comerçamos, escreva seu nome:")
 if nome:
      st.write(nome, ', muito bem!Agora iremos conduzir você para a criação da sua biblioteca.')
 # Você pode passar o caminho direto como string
-st.image(https://offloadmedia.feverup.com/riodejaneirosecreto.com/wp-content/uploads/2023/04/13070413/Real-Gabinete-Portugues.jpg"", caption="Real Gabinete português de Leitura, Rio de Janeiro")
+st.image(https://offloadmedia.feverup.com/riodejaneirosecreto.com/wp-content/uploads/2023/04/13070413/Real-Gabinete-Portugues.jpg", caption="Real Gabinete português de Leitura, Rio de Janeiro")
      
 
 
