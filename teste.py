2bd2e8e66b0c90f371b1a0bf053122538f3c4ff6
rafaellacruzsantoss-spec <rafaellacruzsantoss@gmail.com>
2026-04-29 23:26:45 -0300

    Update teste.py

--- teste.py
+++ teste.py
@@ -1,10 +1,10 @@
 import streamlit as st
 st.title('You to know')
-st.write("Seja bem vindo ao mais novo aplicativo que analisa a estrutura da rede social na era digital!")
+st.write("Seja bem vindo ao mais novo aplicativo que analisa a estrutura da rede social, youtube, na era digital!")
 from PIL import Image
 
 # Você pode passar o caminho direto como string
-st.image("https://gkpb.com.br/22302/novo-logo-youtube/", caption="Primeira rede social que permitiu a atuação do audiovisual na internet")
+st.image("https://gkpb.com.br/wp-content/uploads/2017/08/novo-logo-youtube.jpg", caption="Primeira rede social que permitiu a atuação do audiovisual na internet")
 
 nome = st.text_input("Antes de comerçamos, escreva seu nome:")
 if nome:
