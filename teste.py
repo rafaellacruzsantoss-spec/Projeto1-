7b92a66ed70993b37a8851d2ef4353ee90454f97
rafaellacruzsantoss-spec <rafaellacruzsantoss@gmail.com>
2026-04-29 23:05:59 -0300

    Update teste.py

--- teste.py
+++ teste.py
@@ -1,12 +1,12 @@
 import streamlit as st
-st.title('Breath air books')
-st.write("Seja bem vindo ao aplicativo de encontrar sua versão literária!")
+st.title('To be read')
+st.write("Seja bem vindo ao mais novo aplicativo que tem como intuito encontrar a sua versão literária!")
 from PIL import Image
 
 # Você pode passar o caminho direto como string
 st.image("https://cdn-ileapbh.nitrocdn.com/awswdmxduTjCKQiPPVuNWTjlobpOKWLT/assets/images/optimized/rev-3155ec0/aguiarbuenosaires.com/wp-content/uploads/2022/03/livraria-ateneo.jpg", caption="El Ateneo, uma das livrarias mais belas do mundo")
 
-nome = st.text_input("Digite um lugar que te inspire")
+nome = st.text_input("Antes de comerçamos, gostariamos de saber o nome do usuário")
 if nome:
-     st.write(nome, 'parece ser incrível')
+     st.write(nome, 'muito bem!Iremos agora ')
 
