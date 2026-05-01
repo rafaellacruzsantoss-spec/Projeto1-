f0001fad8c2242b15a71233f61745fd994d45e59
rafaellacruzsantoss-spec <rafaellacruzsantoss@gmail.com>
2026-04-30 23:09:59 -0300

    Update teste.py

--- teste.py
+++ teste.py
@@ -8,14 +8,6 @@ if nome:
 # Você pode passar o caminho direto como string
 st.image("https://offloadmedia.feverup.com/riodejaneirosecreto.com/wp-content/uploads/2023/04/13070413/Real-Gabinete-Portugues.jpg", caption="Real Gabinete português de Leitura, Rio de Janeiro")
 st.write("De início, é possível notar a extrema importancia que a literatura exerce no papel de construção de cada individuo na sociedade. Assim, esse aplicativo busca incentivar não só a leitura mas as consequências que um bom livro pode oferecer")
-import os
-from dotenv import load_dotenv
 
-# Isso procura um arquivo chamado .env no mesmo diretório do script
-load_dotenv()
-api_key = os.getenv("CHAVE_DA_API")
-database_url = os.getenv("DATABASE_URL")
-
-print(f"A chave carregada foi: {api_key}")     
 
 
