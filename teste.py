beac7cb32f74eeaef6a40d3a5c36713fad536a8a
rafaellacruzsantoss-spec <rafaellacruzsantoss@gmail.com>
2026-04-30 23:41:44 -0300

    Update teste.py

--- teste.py
+++ teste.py
@@ -2,8 +2,8 @@ import streamlit as st
 st.title('Guia de Livros ')
 st.write("Seja bem vindo ao mais novo aplicativo que ajudará a você estar mais conectado com pautas literárias!")
 from PIL import Image
- nome = st.text_input("Antes de comerçamos, escreva seu nome:")
- if nome:
+nome = st.text_input("Antes de comerçamos, escreva seu nome:")
+if nome:
  st.write(nome,', muito bem!Agora iremos conduzir você para a criação da sua biblioteca.')
 # Você pode passar o caminho direto como string
  st.image("https://offloadmedia.feverup.com/riodejaneirosecreto.com/wp-content/uploads/2023/04/13070413/Real-Gabinete-Portugues.jpg", caption="Real Gabinete português de Leitura, Rio de Janeiro")
