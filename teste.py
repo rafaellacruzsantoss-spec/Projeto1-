214bf6f9fad94197024eb745d681c0fa212953e9
rafaellacruzsantoss-spec <rafaellacruzsantoss@gmail.com>
2026-04-30 23:40:09 -0300

    Update teste.py

--- teste.py
+++ teste.py
@@ -8,10 +8,10 @@ st.write(nome,', muito bem!Agora iremos conduzir você para a criação da sua b
 # Você pode passar o caminho direto como string
 st.image("https://offloadmedia.feverup.com/riodejaneirosecreto.com/wp-content/uploads/2023/04/13070413/Real-Gabinete-Portugues.jpg", caption="Real Gabinete português de Leitura, Rio de Janeiro")
 st.write("De início, é possível notar a extrema importancia que a literatura exerce no papel de construção de cada individuo na sociedade. Assim, esse aplicativo busca incentivar não só a leitura mas as consequências que um bom livro pode oferecer")
-import requests
-import random
-resposta = requests.get("https://api.nytimes.com/svc/books/v3/lists/{date}/{list}.json")
-dados = resposta.json()
+ import requests
+ import random
+ resposta = requests.get("https://api.nytimes.com/svc/books/v3/lists/{date}/{list}.json")
+ dados = resposta.json()
 
 
 
