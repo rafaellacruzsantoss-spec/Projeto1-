6d24e9159dc2020a9da4721b4d82077857e3ca0a
rafaellacruzsantoss-spec <rafaellacruzsantoss@gmail.com>
2026-05-01 00:10:09 -0300

    Update teste.py

--- teste.py
+++ teste.py
@@ -4,7 +4,7 @@ st.write("Seja bem vindo ao mais novo aplicativo que ajudará a você estar mais
 from PIL import Image
 nome = st.text_input("Antes de comerçamos, escreva seu nome:")
 if nome:
- st.write(nome,', muito bem!Agora iremos conduzir você para a criação da sua biblioteca.')
+ st.write(nome,', muito bem! Agora iremos conduzir você para a criação da sua biblioteca.')
 # Você pode passar o caminho direto como string
  st.image("https://offloadmedia.feverup.com/riodejaneirosecreto.com/wp-content/uploads/2023/04/13070413/Real-Gabinete-Portugues.jpg", caption="Real Gabinete português de Leitura, Rio de Janeiro")
  st.write("De início, é possível notar a extrema importância que a literatura exerce no papel de construção de cada individuo na sociedade. Assim, esse aplicativo busca incentivar o consumo literario critico baseado na revista The New York Times.")
@@ -12,9 +12,10 @@ import requests
 import random
 resposta = requests.get("https://api.nytimes.com/svc/books/v3/lists/{date}/{list}.json")
 dados = resposta.json()
-rank = []
-minha_lista = rank
-len(minha_lista)
+contador = 0
+lista = soup.find_all("rank",["weeks_on_list":"title"])
+
+
 
 
 
