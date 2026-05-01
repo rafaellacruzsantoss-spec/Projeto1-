7c4803677eea3337af78cc3b13ee50750254c0ad
rafaellacruzsantoss-spec <rafaellacruzsantoss@gmail.com>
2026-05-01 19:11:48 -0300

    Update teste.py

--- teste.py
+++ teste.py
@@ -8,7 +8,9 @@ if nome:
 # Você pode passar o caminho direto como string
  st.image("https://offloadmedia.feverup.com/riodejaneirosecreto.com/wp-content/uploads/2023/04/13070413/Real-Gabinete-Portugues.jpg", caption="Real Gabinete português de Leitura, Rio de Janeiro")
  st.write("De início, é possível notar a extrema importância que a literatura exerce no papel de construção de cada individuo na sociedade. Assim, esse aplicativo busca informar e incentivar o consumo literario baseado em dicas para a permanência dessa prática no seu cotidiano.")
-
+ import requests
+ from bs4 import BeatifulSoup
+ url = https://www.nytimes.com/books/best-sellers/
 
 
 
