9e88c06571a30adacb268c41eb54581cc6fc2f68
rafaellacruzsantoss-spec <rafaellacruzsantoss@gmail.com>
2026-04-30 23:28:41 -0300

    Update teste.py

--- teste.py
+++ teste.py
@@ -1,11 +1,11 @@
 import streamlit as st
 st.title('Guia de Livros ')
 st.write("Seja bem vindo ao mais novo aplicativo que ajudará a você estar mais conectado com pautas literárias!")
-respostas= requests.get("https://api.nytimes.com/svc/books/v3/lists/{date}/{list}.json")
+requests.get("https://api.nytimes.com/svc/books/v3/lists/{date}/{list}.json")
 from PIL import Image
 nome = st.text_input("Antes de comerçamos, escreva seu nome:")
 if nome:
-     st.write(nome, ', muito bem!Agora iremos conduzir você para a criação da sua biblioteca.')
+st.write(nome, ', muito bem!Agora iremos conduzir você para a criação da sua biblioteca.')
 # Você pode passar o caminho direto como string
 st.image("https://offloadmedia.feverup.com/riodejaneirosecreto.com/wp-content/uploads/2023/04/13070413/Real-Gabinete-Portugues.jpg", caption="Real Gabinete português de Leitura, Rio de Janeiro")
 st.write("De início, é possível notar a extrema importancia que a literatura exerce no papel de construção de cada individuo na sociedade. Assim, esse aplicativo busca incentivar não só a leitura mas as consequências que um bom livro pode oferecer")
