cdd4cff71b47c0280a6df1298cf38340cefceff7
rafaellacruzsantoss-spec <rafaellacruzsantoss@gmail.com>
2026-05-01 19:27:25 -0300

    Update teste.py

--- teste.py
+++ teste.py
@@ -11,8 +11,7 @@ if nome:
  import requests
  from bs4 import BeatifulSoup
  url = "https://www.nytimes.com/books/best-sellers/"
- st.header = {
-user_agent '=' "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/11"
+ st.header = {user_agent = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/11"}
  site = requests.get(url, headers=header)
  soup = BeautifulSoup(site.content)
  print=soup.find("div",["autor":"nome do livro"]
