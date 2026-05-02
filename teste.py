a8b2760347656eac85cd1d76e65767e59dfeceb2
rafaellacruzsantoss-spec <rafaellacruzsantoss@gmail.com>
2026-05-01 21:54:49 -0300

    Update teste.py

--- teste.py
+++ teste.py
@@ -20,7 +20,7 @@ headers = {"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/
 autor_para_buscar = st.text_input("Digite o nome do autor:")
 # 2. Só executa se a variável não estiver vazia
 if autor_para_buscar:
-    url = f"https://www.goodreads.com?q={autor_para_buscar.replace(' ', '+')}"
+    url = f"https://www.goodreads.com/genres?q={autor_para_buscar.replace(' ', '+')}"
     st.write(f"### 🔗 Link de busca para: {autor_para_buscar}")
     st.link_button("Ver no Goodreads, {url}")
 try:
